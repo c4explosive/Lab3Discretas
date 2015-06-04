d30eb27930e9948775f803cf02850bc1e993508c
c4explosive <ao946391@gmail.com>
2015-06-04 07:51:57 -0500

    Estrutura de conjuntos.

--- lab3.c
+++ lab3.c
@@ -2,23 +2,87 @@
 #include <stdlib.h>
 #include <graphics.h>
 #define sst "H:\\BGI"
+#define TAM 10
+typedef struct cj {
+    int * X;
+    int * Xc;
+} conj;
+void graphic(int *, int *);
+void printj(conj * Y);
+conj * cconj(conj * Z);
+void pcar(int * cj1, int * cjc1, int * cj2, int * cjc2 );
+void nrel();
+void matrel();
+conj * iconj();
 void main()
 {
-    int gd = DETECT,gm,left=100,top=100,right=200,bottom=200,x= 300,y=150,radius=50;
+    int gd = DETECT,gm,i;
+    conj* A=iconj();
+    conj* B=iconj();
+    A=cconj(A);
+    printf("N: %d\n",A->X[0]);
+    //printj(A);
+    //graphic(&gd,&gm);
+}
+
+void graphic(int * gd, int * gm)
+{
+    int left=100,top=100,right=200,bottom=200,x= 300,y=150,radius=50;
     int i;
     char cont='y';
     do
     {
-    	initgraph(&gd,&gm,sst);
+    	initgraph(gd,gm,sst);
     	setcolor(0x1);
-    	/*rectangle(left,top,right,bottom);
-    	circle(left+45,top+34,90);*/
-    	arc(left,top,270,180,90);
     	outtextxy(left + 2, top + 34, "C SDL");
     	outtextxy(left + 2, top + 99, "Salir?");
         cont=getch();
         closegraph();
-	//printf("%c",cont);
     } while(cont == 'y');
     printf("DETECT: 0x%x\n",&gd);
 }
+
+void printj(conj * Y)
+{
+    int i; int vac=1;
+    printf("{");
+    for (i=0;i<TAM;i++)
+    {
+	if(Y->Xc[i])
+	{
+	    printf("%d,",Y->X[i]);
+	    vac=0;
+	}
+    }
+    if (vac)	
+	printf("}\n");
+    else
+    	printf("\b}\n");
+}
+void pcar(int * cj1, int * cjc1, int * cj2, int * cjc2 )
+{
+    int i, j;
+    
+}
+conj * cconj(conj * Z)
+{
+    int i;
+    for (i=0;i<16;i++)
+    {
+	/*if(i%2==0)
+	{*/
+	    Z->X[i]=i+2;
+	    Z->Xc[i]=1;
+	//}
+    }
+    return Z;
+}
+conj * iconj()
+{
+    conj * ccj=malloc(sizeof(conj));
+    ccj->X=malloc(TAM*sizeof(int));
+    ccj->Xc=malloc(TAM*sizeof(int));
+    *ccj->Xc=0;
+    return ccj;
+}
+
