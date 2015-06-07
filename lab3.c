260c6415041cc94f08cb4d85fcec6baee1a0c2ed
c4explosive <ao946391@gmail.com>
2015-06-07 15:10:46 -0500

    Dìgrafo, cond. rel. return biarray, etc

--- lab3.c
+++ lab3.c
@@ -5,20 +5,22 @@
 #include <math.h>
 #define sst "H:\\BGI"
 #define TAM 20 //Se puede optimizar, TODO: cambiar por tamaño no fijo (REAL).
-typedef struct cj {
+typedef struct cj 
+{
     int * X;
     int * Xc;
 } conj;
 ////////////F(x) Principales/////////  
-void graphic(int *, int *,conj * A);
+void graphic(int *, int *,conj *,conj *);
 void printj(conj * Y);
 conj * cconj(conj * Z);
-int ** nrel(conj *,conj *);
+int ** nrel(conj *,conj *,int);
 void matrel();
 void pcar(conj * Y,conj * Z);
 //////////F(x) Auxiliares/////////////////
 char * intostr(int);
 void triangle(int, int);
+void pbiarray(int **, int k);
 conj * iconj();
 /////////////////////////////////////////
 void main()
@@ -29,21 +31,36 @@ void main()
     A=cconj(A);
     B=A;
     //printf("N: %d\n",A->X[0]);
-    printj(A);
-    printj(B);
+    printf("A=");printj(A);
+    printf("B=");printj(B);
     pcar(A,B);
-    nrel(A,B);
+    //nrel(A,B,1);
     getch();
-    graphic(&gd,&gm,A);
+    graphic(&gd,&gm,A,B);
 }
 ////////////////////////////GRAPHIC MODE////////////////////////////////////////
-void graphic(int *gd, int *gm,conj * A)
+void graphic(int *gd, int *gm,conj * A,conj * B)
 {
     int left=100,top=100,right=200,bottom=200,x= 300,y=150,radius=50;
-    int i,k=0;
+    int i,j,k=0,l=0,p1,p2;
     int Xpos[2][7]={{23,45,67,90,178,152,255},
 		    {45,140,89,38,17,142,105}};
+    int** Pos=malloc(sizeof(int*));
+    for(i=0;i<2;i++)
+	Pos[i]=malloc(7*sizeof(int));
+    int ** rel=nrel(A,B,1);
+    for(k=0;k<TAM;k++)
+    {
+	if(A->Xc[k])
+	{
+    	    Pos[0][l]=A->X[k];
+	    Pos[1][l]=l;
+	    l++;
+	}
+    }
+    //pbiarray(Pos,l);
     char cont='y';
+    k=0;
     do
     {
     	initgraph(gd,gm,sst);
@@ -57,18 +74,55 @@ void graphic(int *gd, int *gm,conj * A)
 	    }
 		
 	}
-	for (i=0;i<7;i++)
+	l=k=0;
+	l++;
+	for (i=1;i<rel[0][0]-1;i++)
 	{
-	    if(i==6)
-	    	break;
+	    for (j=0;j<7;j++)
+	    {
+	    	if( rel[0][i] == Pos[0][j])
+		{
+		    p1=Pos[1][j];
+		}
+		if( rel[1][i] == Pos[0][j])
+		{
+		    p2=Pos[1][j];
+		}
+
+	    }
+	    //printf("n: %d->%d\n",Xpos[0][p1],Xpos[0][p2]);
+	    if (!(rel[0][i]==rel[1][i]))
+	    {
+    		setcolor(l);
+	    	line(left+Xpos[0][p1]-5,top+ Xpos[1][p1]+2,left+Xpos[0][p2]-5,top+ Xpos[1][p2]+2);
+		if(l!=10)
+		    l++;
+		else
+		    l=1;
+    		setcolor(0xE);
+		triangle(left+Xpos[0][p2]-5,top+ Xpos[1][p2]);
+	    }
 	    else
 	    {
-    		setcolor(0x1);
-	    	line(left+Xpos[0][i]-5,top+ Xpos[1][i]+2,left+Xpos[0][i+1]-5,top+ Xpos[1][i+1]+2);
-    		setcolor(0x4);
-		triangle(left+Xpos[0][i+1],top+ Xpos[1][i+1]);
-	    }	
+    		setcolor(l);
+		circle(left+Xpos[0][p1]+5,top+Xpos[1][p1]+5,20);
+		if(l!=10)
+		    l++;
+		else
+		    l=1;
+	    }
+	    	
 	}
+	for (i=0;i<TAM;i++)
+	{
+	    if(A->Xc[i])
+	    {
+    	    	outtextxy(left + Xpos[0][k], top + Xpos[1][k], intostr(A->X[i]));
+	    	k++;
+	    }
+		
+	}
+
         cont=getch();
         closegraph();
     } while(cont == 'y');
@@ -116,7 +170,7 @@ void printj(conj * Y)
 void pcar(conj * Y,conj * Z)
 {
     int i, j;
-    printf("{");
+    printf("AxB={");
     for(i=0;i<TAM;i++)
     {
 	//printf("DEBUG: (%d) -> POS: %d\n",Y->X[i],i);
@@ -136,21 +190,59 @@ void pcar(conj * Y,conj * Z)
         
 }
 /////////////////////////Condición para Relación//////////////////////////
-int ** nrel(conj *Y,conj *Z)
+int ** nrel(conj *Y,conj *Z,int s)
 {
     int i,j,k=0;
     int **pares=malloc(sizeof(int*));
     for(k=0;k<2;k++)
 	pares[k]=malloc(49*sizeof(int));
+    k=1;
+    if(s)
+    	printf("R={");
+    for(i=0;i<TAM;i++)
+    {
+	if (Y->Xc[i])
+	{
+	    for(j=0;j<TAM;j++)
+	    {
+		if(Z->Xc[j])
+		{
+		    if((Y->X[i]*Z->X[j])<(pow(Y->X[i],2)))
+		    //if(Y->X[i]%Z->X[j]==0)
+		    {
+			if(s)
+			    printf("(%d,%d),",Y->X[i],Z->X[j]); //TODO: ¿puede ser independiente o depende del producto cartesiano?
+			pares[0][k]=i;
+			pares[1][k]=j;
+			k++;
+		    }
+		}
+	    }
+	}
+    }
+    if(s)
+    	printf("\b}\n");
+    k++;
+    pares[0][0]=k;
+    pares[1][0]=k;
+    k--;
+    pares[0][k]=1000;
+    pares[1][k]=1000;
+    //pbiarray(pares,pares[0][0]);
+    return pares;
+}
+
+void pbiarray(int ** biarray, int k)
+{
+    int j,i;
     for(i=0;i<2;i++)
     {
 	for(j=0;j<k;j++)
 	{
-	    printf("%d ",pares[i][j]);
+	    printf("%d\t",biarray[i][j]);
 	}
 	printf("\n");
     }
-
 }
 ///////////////////////////////Cargar Conjunto/////////////////////////////
 conj * cconj(conj * Z)
